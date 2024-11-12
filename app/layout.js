090fcb8a3b42740a31c5752850bf805073a8125e
Katerina <katy-lola@mail.ru>
2024-11-12 20:40:02 +0300

    fix

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,5 @@
-import localFont from "next/font/local";
+import Head from "next/head";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +9,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
         {children}
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,95 +1,78 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import ImageOne from'./image/Image.png'
+import Image from "next/image";
+import ImageTwo from'./image/Image (1).png'
+import ImageThree from'./image/Image (2).png'
+import ShoppingCart from './image/shoppingCart.png'
+import Arrow from './image/arrow.png'
 
 export default function Home() {
+  const steps = [
+    {
+      number: "1",
+      title: 'Получите</br> партнерскую ссылку',
+      image: ImageOne,
+      alt: "stepOne",
+      containerStyle: styles.containerStepOne,
+      stepNumberStyle: styles.stepNumberOne,
+      stepImageStyle: styles.stepImageOne,
+    },
+    {
+      number: "2",
+      title: "Расскажите</br> о IIII.COM",
+      image: ImageTwo,
+      alt: "stepTwo",
+      containerStyle: styles.containerStepTwo,
+      stepNumberStyle: styles.stepNumberTwo,
+      stepImageStyle: styles.stepImageTwo,
+    },
+    {
+      number: "3",
+      title: "Получайте хороший процент</br> от покупок по вашей ссылке!",
+      image: ImageThree,
+      alt: "stepThree",
+      containerStyle: styles.containerStepThree,
+      stepNumberStyle: styles.stepNumberThree,
+      stepImageStyle: styles.stepImageThree,
+    },
+  ];
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className={styles.titleBox}>
+          <h3 className={styles.title}>Зарабатывайте вместе с нами!</h3>
+        </div>
+        <div className={styles.container}>
+          {steps.map((step, index) => (
+            <>
+              <div className={step.containerStyle} key={`container-${index}`}>
+                <div className={styles.containerBox}>
+                  <div className={step.stepNumberStyle}>{step.number}</div>
+                  <h3
+                  className={styles.stepTitle}
+                  dangerouslySetInnerHTML={{ __html: step.title }}
+                />
+                </div>
+                <div className={step.stepImageStyle}>
+                  <Image src={step.image} alt={step.alt}/>
+                </div>
+              </div>
+              {index < steps.length - 1 && (
+                <div className={index === 0 ? styles.arrowOne : styles.arrowTwo} key={`arrow-${index}`}>
+                  <Image src={Arrow} alt="arrow"/>
+                </div>
+              )}
+            </>
+          ))}
+        </div>
+        <div className={styles.btnBox}>
+          <button className={styles.btn}>
+            <Image className={styles.btnImage} src={ShoppingCart} alt="Shopping Cart Icon" width={24} height={24} />
+            Статья партнером
+          </button>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
